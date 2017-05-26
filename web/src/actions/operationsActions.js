b6a18b83b2c7ebc80abc7b051581aba2b633f90e
dieggger <dieggger@gmail.com>
2017-05-26 16:04:00 -0500

    Added post to api using super agent

--- web/src/actions/operationsActions.js
+++ web/src/actions/operationsActions.js
@@ -2,18 +2,16 @@
 const Request = require('superagent');
 
 export function postOperation(url) {
-  debugger;
   return dispatch => {
     return new Promise(function (resolve, reject) {
       Request
-        .post(`https://requestb.in/1kyyxik1?inspect`)
+        .post(`http://localhost:3000/transaction`)
         .send(url)
-        .withCredentials()
-        .end(function (err, res) {
+        .end((err, res) => {
           if (err) {
             return reject(err);
           }
-          return resolve(res.url);
+          return resolve(res.body);
         });
     });
   };
